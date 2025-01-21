3010af8dc9378f2f4abeecd5f4a6ddbd14283c9c
krishnatejak <kkt.iitm@gmail.com>
2025-01-21 10:43:47 +0530

    Add App component

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import RuleTree from './RuleTree'
+import './index.css'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 py-8">
+      <RuleTree />
+    </div>
+  )
+}
+
+export default App
